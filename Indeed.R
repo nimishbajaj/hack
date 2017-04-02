9ac51eecf17c49457940e04c40fb7372b0fe6e55
Nimish <nimishbajaj@hotmail.com>
2017-04-03 02:45:36 +0530

    Preparing Data

--- Indeed.R
+++ Indeed.R
@@ -1,2 +1,6 @@
 indeed = read.table(file = "train.tsv", sep = '\t', header = TRUE, stringsAsFactors = FALSE, quote = "\"")
-indeedPart = cbind(indeed[grep("part|full", indeed$tags),])
+indeedPart = indeed
+indeedPart$Part = ifelse( grepl("part",indeedPart$tags), 1, 0)
+indeedPart$Full = ifelse( grepl("full",indeedPart$tags), 1, 0)
+indeedPart$tags = NULL
+write.csv(indeedPart,"indeedPart.csv")
